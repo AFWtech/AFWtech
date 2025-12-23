e17ae59fd9ebd14388c886f5a11210a548982905
AFWtech <8583o77883@gmail.com>
2025-12-23 21:21:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function callNow() {
+    window.location.href = "tel:+91XXXXXXXXXX";
+}
+
+function searchItem() {
+    const query = document.getElementById("searchBox").value.toLowerCase();
+    alert("Searching for: " + query + " (Feature coming soon!)");
+}
